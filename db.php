d577c9c663fa696319a510fd1558fb4d3735f4b1
SebastianRetamal777 <se.retamalt@alumnos.duoc.cl>
2022-11-11 10:09:26 -0300

    ahora abre home

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conexion=mysqli_connect("localhost","root","","seguridad");
+$conexion=mysqli_connect("localhost","adminc","","seguridad");
 
 
 ?>
--- validar.php
+++ validar.php
@@ -4,7 +4,7 @@ $contraseña=$_POST['contraseña'];
 session_start();
 $_SESSION['usuario']=$usuario;
 
-$conexion= mysqli_connect("localhost","adminc","asdf","seguridad");
+$conexion= mysqli_connect("localhost","root","","seguridad");
 
 $consulta="SELECT usuario, contraseña FROM usuarios where usuario='$usuario' and contraseña='$contraseña'";
 $resultado=mysqli_query($conexion,$consulta);
