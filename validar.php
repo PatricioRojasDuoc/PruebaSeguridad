c84c64c819b3f5b2eaa605044ef8585bc5f72f42
SebastianRetamal777 <74253765+SebastianRetamal777@users.noreply.github.com>
2022-11-12 19:37:53 -0300

    Add files via upload

--- validar.php
+++ validar.php
@@ -4,32 +4,21 @@ $contraseña=$_POST['contraseña'];
 session_start();
 $_SESSION['usuario']=$usuario;
 
-$conexion=mysqli_connect("54.226.41.52","Administrator","","seguridad");
+$conexion= mysqli_connect("localhost","root","","seguridad");
 
-$consulta="SELECT usuario, contraseña  FROM usuarios 
-where usuario='$usuario' and contraseña='$contraseña'";
-
+$consulta="SELECT usuario, contraseña FROM usuarios where usuario='$usuario' and contraseña='$contraseña'";
 $resultado=mysqli_query($conexion,$consulta);
 
 $filas=mysqli_num_rows($resultado);
 
 if($filas){
-    if (isset($_SESSION['$usuario'])){
     header("location:home.php");
-    }else{
-        ?>
-        <?php
-        include("index.php");
-        ?>
-        <h1>Error en la Autenticacion2</h1>
-        <?php
-    }
 }else{
     ?>
     <?php
     include("index.php");
     ?>
-    <h1>Error en la Autenticacion</h1>
+    <h1 class="bad">Error en la Autenticacion</h1>
     <?php
 }
 
